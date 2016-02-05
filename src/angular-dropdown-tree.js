f13b1eba5b4637bd89a860386b6b6f3d833760b6
Patrick <ammeyjohn@gmail.com>
2016-02-05 21:33:57 +0800

    Complete: make multiple selection available.

--- src/angular-dropdown-tree.js
+++ src/angular-dropdown-tree.js
@@ -9,20 +9,16 @@
         }
     }
 
-    function uiDropdownTreeCtrl() {
-
-    }
-
     function uiDropdownTreeDirective() {
         return {
             restrict: 'EA',
             replace: true,
             scope: {
+                multiple: '@dropdownTreeMultiple',
                 treeData: '=dropdownTreeData'
             },
             require: 'ngModel',
             templateUrl: '../src/template.html',
-            controller: 'uiDropdownTreeCtrl',
             link: function link(scope, elm, attrs, controller) {
 
                 // 为每个控件生成唯一的id编号
@@ -33,12 +29,36 @@
                 var tree = null;
 
                 var getOption = function(data) {
-                	var opt = angular.copy(default_option);
+                    var opt = angular.copy(default_option);
                     opt = angular.extend({}, opt);
                     if (scope.treeData) {
                         opt.core.data = data;
                     }
+                    // 是否支持多选
+                    if (isMultiple()) {
+                        opt.plugins.push('checkbox');
+                    }
                     return opt;
+                }
+
+                var showSelectedTitle = function(selected) {
+                    var title = '';
+
+                    if (selected.length > 3) {
+                        title = '选中' + selected.length + '项';
+                    } else {
+                        for (var i = 0; i < selected.length; i++) {
+                        	var item = selected[i];
+                        	var text = item;
+                        	if(item.text) { text = item.text; }
+                        	else if (item.title) { text = item.title; }
+                        	else if (item.id) { text = item.id; }
+                            title += ',' + text;
+                        }
+                        title = title.substr(1);
+                    }
+
+                    scope.dropdown_title = title;
                 }
 
                 // 初始化jstree组件
@@ -46,22 +66,35 @@
 
                     // 初始化树控件
                     tree = $(elm).find('div.dropdown-menu div');
-                    if(tree.length == 0) {
-                    	tree = $('<div />');
-                    	$(elm).find('div.dropdown-menu').append(tree);
+                    if (tree.length == 0) {
+                        tree = $('<div />');
+                        $(elm).find('div.dropdown-menu').append(tree);
                     }
 
-                   	// 获取配置
+                    // 获取配置
                     var opt = getOption(data);
 
                     // 初始化树配置
                     tree
                         .on('changed.jstree', function(e, data) {
-                        	var selected_node = data.instance.get_node(data.selected);
-                            scope.dropdown_title = selected_node.text;
-                         	controller.$setViewValue(selected_node.original);
-                            scope.$apply();
-                            $(elm).removeClass('open');
+                            var selected_nodes = [];
+                            for (var selected_id in data.selected) {
+                                var node = data.instance.get_node(data.selected[selected_id]);
+                                selected_nodes.push(node.original);
+                            }
+                            scope.$apply(function() {
+
+                                // 设置ngModel
+                                controller.$setViewValue(selected_nodes);
+
+                                // 显示选中项
+                                showSelectedTitle(selected_nodes);
+                            });
+
+                            if (!isMultiple()) {
+                                // 关闭dropdownMenu
+                                $(elm).removeClass('open');
+                            }
                         })
                         .on('click', function(e) {
                             e.stopPropagation();
@@ -70,16 +103,20 @@
                 }
 
                 var destoryTree = function() {
-                	if(tree) {
-                		tree.jstree('destroy');
-                		tree.remove();
-                		tree = null
-                	}
+                    if (tree) {
+                        tree.jstree('destroy');
+                        tree.remove();
+                        tree = null
+                    }
+                }
+
+                var isMultiple = function() {
+                    return scope.multiple.toLowerCase() == 'true';
                 }
 
                 scope.$watch('treeData', function(newData) {
                     if (newData) {
-                    	destoryTree();
+                        destoryTree();
                         initTree(newData);
                     }
                 });
@@ -89,7 +126,6 @@
 
     //// Angular Code ////
     var module = angular.module('uiDropdownTree', []);
-    module.controller('uiDropdownTreeCtrl', uiDropdownTreeCtrl);
     module.directive('uiDropdownTree', uiDropdownTreeDirective);
 
 })(jQuery, angular);
